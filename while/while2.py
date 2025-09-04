34572684a6813c67bed403ee95d0133370c7ac40
hyer11111111-beep <hyer11111111@gamil.com>
2025-09-04 15:45:56 +0900

    first commit

---
+++ while/while2.py
@@ -0,0 +1,38 @@
+# 반복문 
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+
+# 반복하고 싶은 코드
+# : print(?)
+# 반복 횟수 
+# 10번
+i = 0
+while 1 < 10:
+   print("안녕하세요")
+
+# i가 0부터 9까지 변화하면서 총 20번 반복
+i = 1
+while 1 <= 11:
+   print("i")
+   i=i+i
+
+   i = 1
+while 1 <= 11:
+   print("i")
+   i=i+i
+
+while i < 11:
+   print(I)
+   i=i+1=
+
+# i가 1부터 1씩 증가가 되면서
+# 10년 되면 반복문이 종료될까 
+
